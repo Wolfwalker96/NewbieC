7eae57abcd30a53743ad9b60d163af03b5de9ee4
Dylan Santos de Pinho <dylan.santosdepinho@he-arc.ch>
2017-12-20 08:36:30 +0100

    Base du projet

---
+++ lex.py
@@ -0,0 +1,53 @@
+import ply.lex as lex
+
+tokens = (
+   'NUMBER',
+   'PLUS',
+   'MINUS',
+   'MULT',
+   'DIV',
+   'MOD',
+   'LTH',
+   'GTH',
+   'EQU',
+   'COND'
+)
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_MULT = r'\*'
+t_DIV = r'/'
+t_MOD = r'%'
+t_LTH = r'<'
+t_GTH = r'>'
+t_EQU = r'='
+t_COND = r'\?'
+
+
+def t_NUMBER(t):
+	r'\d+'
+	t.value = int(t.value)
+	return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+t_ignore  = ' \t'
+
+def t_error(t):
+    print ("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lex.lex()
+
+if __name__ == "__main__":
+	import sys
+	prog = open(sys.argv[1]).read()
+
+	lex.input(prog)
+
+	while 1:
+		tok = lex.token()
+		if not tok: break
+		print ("line %d: %s(%s)" % (tok.lineno, tok.type, tok.value))
---
+++ parser.py
@@ -0,0 +1,30 @@
+import ply.yacc as yacc
+from lex import tokens
+
+operations = {
+    '+': lambda x,y: x+y,
+    '-': lambda x,y: x-y,
+    '*': lambda x,y: x*y,
+    '/': lambda x,y: x/y
+}
+
+def p_expression_num(p):
+    'expression : NUMBER'
+    p[0] = p[1]
+
+def p_expression_op(p):
+    '''expression : expression ADD_OP expression
+        | expression MULT_OP'''
+    p[0] = operations[p[2]](p[1],p[3])
+
+def p_error(p):
+    print("Syntax error in line %d" % p.lineno)
+    yacc.errok()
+
+yacc.yacc(outputdir='generated')
+
+if __name__ == "__main__":
+    import sys
+    prog = open(sys.argv[1]).read()
+    result = yacc.parse(prog)
+    print(result)
